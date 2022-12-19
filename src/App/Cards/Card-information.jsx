2b556e1c00384ad199a9daefba857bc38a2417e9
SnakeUY <101599259+SnakeUY@users.noreply.github.com>
2022-12-19 20:58:00 -0300

    Finally fixed error 404 and loading

--- src/App/Cards/Card-information.jsx
+++ src/App/Cards/Card-information.jsx
@@ -5,44 +5,45 @@ import Error404 from "../Pokemon-Search/Error404";
 import Loader from "../Loader/Loader";
 import Moves from "./Moves";
 import TableType from "./Table-type";
+import { useReducer } from "react";
 
 const CardInformation = () =>{
+    const [state,setState] = useReducer((state,newState)=>{return{...state,...newState}},{isLoading:true})
     const id = useParams().id
     const [pokemon,setPokemon] = useState([])
-    const [isLoading, setIsLoading] = useState(true)
-    const [poke,setPoke] = useState([])
-    useEffect(()=>{
-        async function fetchData(){
-          getPokemons(pokemon=>{
-            setPokemon(pokemon)
-            console.log(pokemon)
-          })
-        }
+
+    async function fetchData(){
+        await getPokemons(pokemon=>{
+          setPokemon(pokemon)
+        })
+      }
+
+      useEffect(()=>{
         fetchData()
-      },[id])
+      },[])
 
     useEffect(()=>{
-        setPoke(pokemon.find((candidate)=>candidate.id ==id))
-    },[pokemon])
-
-    useEffect(()=>{
-        setIsLoading(false)
-    },[poke])
+        let aux = pokemon.find((candidate)=>candidate.id ==id)
+        if(aux){
+            setState({pokemon: aux,isLoading:false})
+        }else if(pokemon.length > 0){
+                setState({isLoading:false})
+            }
+      },[id,pokemon])
 
     function getZeroes(number){
         return number.toString().padStart(3,'0')
     }
-    let index = pokemon.indexOf(poke)
-
+    let index = pokemon.indexOf(state.pokemon)
 
     return( 
     <>
-    {(isLoading)?
+    {(state.isLoading)?
        <Loader/> :
-        <>
-        {(!poke)?
-            <Error404/> :
-            <div className={`${poke.idtype_types[0].type.toLowerCase()} big-card-container`}>
+       <>
+        {(!state.pokemon)?
+        <Error404/>:
+            <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()} big-card-container`}>
 
                 <div className= 'bgrn-pokeball'>
                     <img src="./Imagenes/Pokeball.png" alt="" />
@@ -53,10 +54,10 @@ const CardInformation = () =>{
                     <Link to={'/'}> 
                         <img className='back-arrow' src="./Imagenes/arrow-left.svg" alt=""/>
                     </Link>
-                        <span>{poke.name}</span>
+                        <span>{state.pokemon.name}</span>
                     </div>
                     <div className='pokemon-number'>
-                        <span>#{getZeroes(poke.id)}</span>
+                        <span>#{getZeroes(state.pokemon.id)}</span>
                     </div>
                 </div>
                 
@@ -69,21 +70,25 @@ const CardInformation = () =>{
                         )
                     }
                 
-                    <img className={`${poke.name.toLowerCase()}-image`} src={poke.pokeurl} style={{height: "200px",weight:"200px"}}/>
-                
+                    <img className={`${state.pokemon.name.toLowerCase()}-image`} src={state.pokemon.pokeurl} style={{height: "200px",weight:"200px"}}/>
+                    <div>
                     {
+                        
                         pokemon[index+1] &&  (
+                            
                     <Link to={'/'+pokemon[index+1].id}>
                             <img className="frame-arrow" src="./Imagenes/Frame.svg" alt="" />
                     </Link> 
+                    
                     )
                 }
+                </div>
                 </div>
 
                 <div className='container-type'>
                     <div className='table-type'>
                             {
-                            poke.idtype_types.map((type)=>{
+                            state.pokemon.idtype_types.map((type)=>{
                                 return(
                                 <TableType
                                 type={type.type} />)
@@ -91,13 +96,13 @@ const CardInformation = () =>{
                         }
                     </div>
 
-                    <span className={`${poke.idtype_types[0].type.toLowerCase()}-color-txt about`}>About</span>
+                    <span className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-color-txt about`}>About</span>
 
                     <div className='weight-height-moves'>
                         <div className='body-stat'>
                             <div className='measure-box'>
                                 <img src="./Imagenes/Weight.svg" alt="" />
-                                <span className='measure'>{poke.weight}</span>
+                                <span className='measure'>{state.pokemon.weight}</span>
                             </div>
                             <span className='span-stat'>Weight</span>
                         </div>
@@ -105,15 +110,15 @@ const CardInformation = () =>{
                         <div className='body-stat middle-stat'>
                             <div className='measure-box'>
                                 <img src="./Imagenes/Height.svg" alt="" />
-                                <span className='measure'>{poke.height}</span>
+                                <span className='measure'>{state.pokemon.height}</span>
                             </div>
                             <span className='span-stat'>Height</span>
                         </div>
 
-                        <div className={`${poke.name.toLowerCase()}-moves body-stat`}>
+                        <div className={`${state.pokemon.name.toLowerCase()}-moves body-stat`}>
                             <div className='measure-box moves'>
                             {
-                                poke.idmove_moves.map((move)=>{
+                                state.pokemon.idmove_moves.map((move)=>{
                                     return(
                                         <Moves
                                         move={move.move}/>)
@@ -126,12 +131,12 @@ const CardInformation = () =>{
 
                     <div className='stats-container'>
                     <div className='description-box'>
-                            <p className='description'>{poke.description}</p>
+                            <p className='description'>{state.pokemon.description}</p>
                     </div>
-                        <span className={`${poke.idtype_types[0].type.toLowerCase()}-color-txt`}>Base Stats</span>
+                        <span className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-color-txt`}>Base Stats</span>
                             <div className='base-stats-container'>
                                 <div className='stat-box-left'>
-                                    <div className={`${poke.idtype_types[0].type.toLowerCase()}-color-txt stat-composition`}>
+                                    <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-color-txt stat-composition`}>
                                         <span>HP</span>
                                         <span>ATK</span>
                                         <span>DEF</span>
@@ -141,45 +146,45 @@ const CardInformation = () =>{
                                     </div>
                                     
                                     <div className='stat-number'>
-                                        <span>{getZeroes(poke.hp)}</span>
-                                        <span>{getZeroes(poke.atk)}</span>
-                                        <span>{getZeroes(poke.def)}</span>
-                                        <span>{getZeroes(poke.satk)}</span>
-                                        <span>{getZeroes(poke.sdef)}</span>
-                                        <span>{getZeroes(poke.spd)}</span>
+                                        <span>{getZeroes(state.pokemon.hp)}</span>
+                                        <span>{getZeroes(state.pokemon.atk)}</span>
+                                        <span>{getZeroes(state.pokemon.def)}</span>
+                                        <span>{getZeroes(state.pokemon.satk)}</span>
+                                        <span>{getZeroes(state.pokemon.sdef)}</span>
+                                        <span>{getZeroes(state.pokemon.spd)}</span>
                                     </div>
                                 </div>
 
                                 <div className='stat-bar-container'>
-                                    <div className={`${poke.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
-                                        <div style={{width:`${(poke.hp*100)/200}%`}} className={`${poke.idtype_types[0].type.toLowerCase()} inner-bar`}/>
+                                    <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
+                                        <div style={{width:`${(state.pokemon.hp*100)/200}%`}} className={`${state.pokemon.idtype_types[0].type.toLowerCase()} inner-bar`}/>
                                     </div>
 
-                                    <div className={`${poke.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
-                                        <div style={{width:`${(poke.atk*100)/200}%`}} className={`${poke.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
+                                    <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
+                                        <div style={{width:`${(state.pokemon.atk*100)/200}%`}} className={`${state.pokemon.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
                                     </div>
 
-                                    <div className={`${poke.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
-                                        <div style={{width:`${(poke.def*100)/200}%`}} className={`${poke.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
+                                    <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
+                                        <div style={{width:`${(state.pokemon.def*100)/200}%`}} className={`${state.pokemon.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
                                     </div>
                                     
-                                    <div className={`${poke.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
-                                        <div style={{width:`${(poke.satk*100)/200}%`}} className={`${poke.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
+                                    <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
+                                        <div style={{width:`${(state.pokemon.satk*100)/200}%`}} className={`${state.pokemon.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
                                     </div>
 
-                                    <div className={`${poke.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
-                                        <div style={{width:`${(poke.sdef*100)/200}%`}} className={`${poke.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
+                                    <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
+                                        <div style={{width:`${(state.pokemon.sdef*100)/200}%`}} className={`${state.pokemon.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
                                     </div>
 
-                                    <div className={`${poke.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
-                                        <div style={{width:`${(poke.spd*100)/200}%`}} className={`${poke.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
+                                    <div className={`${state.pokemon.idtype_types[0].type.toLowerCase()}-bar-bgrn stat-bar`}>
+                                        <div style={{width:`${(state.pokemon.spd*100)/200}%`}} className={`${state.pokemon.idtype_types[0].type.toLowerCase()} inner-bar`}></div>
                                     </div>
                                 </div>
                             </div>
                     </div>
                 </div>
             </div>
-        }
+    }
     </>}
     </>
     )
